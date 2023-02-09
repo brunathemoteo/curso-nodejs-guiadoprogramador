0502d9a8a93c49eba903cb11fbc75995b49c2b31
brunathemoteo <brunathemoteo@gmail.com>
2023-02-09 01:05:15 -0300

    listando categorias

--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,17 @@ router.get('/posts', (req, res) => {
 })
 
 router.get('/categorias', (req, res) => {
-  res.render('admin/categorias')
+  Categoria.find()
+    .sort({ date: 'desc' })
+    .then((categorias) => {
+      res.render('admin/categorias', {
+        categorias: categorias.map((Categoria) => Categoria.toJSON())
+      })
+    })
+    .catch((err) => {
+      req.flash('error_msg', 'Houve um erro ao listas as categorias')
+      res.redirect('/admin')
+    })
 })
 
 router.get('/categorias/add', (req, res) => {
