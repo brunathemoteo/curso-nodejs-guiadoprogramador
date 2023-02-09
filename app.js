8781f71ea88d0750c621a9d4fb6af286be1d0bc1
brunathemoteo <brunathemoteo@gmail.com>
2023-02-08 23:52:51 -0300

    validacao de formularios no express.js

--- app.js
+++ app.js
@@ -5,14 +5,36 @@ const app = express()
 const admin = require('./routes/admin')
 const path = require('path')
 const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 // Configurações
+
+//Sessão
+app.use(
+  session({
+    secret: 'cursodenode',
+    resave: true,
+    saveUninitialized: true
+  })
+)
+app.use(flash())
+
+// Middleware
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  next()
+})
+
 // Body parser
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
 //Handlebars
 app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
 //Mongoose
 mongoose.Promise = global.Promise
 mongoose.set('strictQuery', true)
@@ -24,10 +46,13 @@ mongoose
   .catch((err) => {
     console.log('Erro ao se conectar: ' + err)
   })
+
 //Public
 app.use(express.static(path.join(__dirname, 'public')))
+
 // Rotas
 app.use('/admin', admin)
+
 // Outros
 const PORT = 8081
 app.listen(PORT, () => {
